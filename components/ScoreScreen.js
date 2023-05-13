ae7dcb4d413c07952c3dbd0479925a3f95555c79
Aleksi Viitanen <n1vial00@students.oamk.fi>
2023-05-13 19:36:46 +0300

    Some rollback.

--- components/ScoreScreen.js
+++ components/ScoreScreen.js
@@ -1,18 +1,18 @@
-import { Provider, MD3DarkTheme } from 'react-native-paper';
-import { View, Table, Row, Rows } from 'react-native-table-component';
+import { Provider, Text, MD3DarkTheme } from 'react-native-paper';
+import { View } from 'react-native';
 import Styles from '../styles/Styles';
 
 const ScoreScreen = (props) => {
-  const tableData = props.scores.map((score) => [score.name, score.scoreData]);
-
   return (
     <Provider theme={MD3DarkTheme}>
       <View style={Styles.mainContainer}>
         <Text style={Styles.top}>TOP 10 PLAYERS!</Text>
-        <Table>
-          <Row data={['Player', 'Score']} style={Styles.tableHeader} textStyle={Styles.tableHeaderText} />
-          <Rows data={tableData} textStyle={Styles.tableRowText} />
-        </Table>
+        <Text>Player - Score</Text>
+        {props.scores?.map((score, j) => (
+          <Text key={j}>
+            {score.name} - {score.scoreData}
+          </Text>
+        ))}
       </View>
     </Provider>
   );
